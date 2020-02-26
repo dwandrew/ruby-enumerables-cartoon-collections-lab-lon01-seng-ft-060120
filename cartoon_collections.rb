9d85b049b6ce06f128f4220df303bb7ca4984edc
Daniel Andrew <dwandrew@hotmail.com>
2020-02-26 19:05:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def summon_captain_planet(veggies)
 end
 
 def long_planeteer_calls(calls)
-  calls.select {|calls| call.length > 4} 
+  calls.select {|calls| calls.length > 4} 
 end
 
 def find_the_cheese# code an argument here
