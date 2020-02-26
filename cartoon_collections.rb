7d863f2eda326b5ef5365d42f45bad2bb7eba754
Daniel Andrew <dwandrew@hotmail.com>
2020-02-26 18:55:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(veggies)
-  veggies. {|veg| veg.capitalize!}
+  veggies.collect {|veg| veg.capitalize! + '!'}
 end
 
 def long_planeteer_calls# code an argument here
