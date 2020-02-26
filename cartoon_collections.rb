6811f32f5984fe968e995b96d34eab25654aac73
Daniel Andrew <dwandrew@hotmail.com>
2020-02-26 19:08:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def long_planeteer_calls(calls)
   calls.any? {|calls| calls.length > 4} 
 end
 
-def find_the_cheese
-  # the array below is here to help
+def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  foods.
 end
